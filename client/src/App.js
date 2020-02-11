4aabe7287f50ae9f28e6948c6d102074af35e23a
justinandrade <j.andrade.john@gmail.com>
2020-02-11 16:14:26 -0500

    Boilerplate created

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <div className="App">
+        <h1>BookR</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
