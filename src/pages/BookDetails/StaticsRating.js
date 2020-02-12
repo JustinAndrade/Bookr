a62833323945681f79ea0deb72fe80885e5f94e9
justinandrade <j.andrade.john@gmail.com>
2020-02-12 18:20:50 -0500

    delted files

--- src/pages/BookDetails/StaticsRating.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Rate } from "antd";
-
-const StaticRating = props => {
-  return <Rate {...props} disabled="true" defaultValue={2} />;
-};
-
-export default StaticRating;
