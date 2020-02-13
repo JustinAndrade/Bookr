644e13084afc1a0ac0f6f95af372ac69b06d8d69
justinandrade <j.andrade.john@gmail.com>
2020-02-13 03:11:52 -0500

    assests fixed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
-import "../App.scss";
+import "../App.css";
 import Navigation from "./Navigation/Navigation";
 
 const Header = () => {
